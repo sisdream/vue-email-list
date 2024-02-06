4ae066b8b420ca5e4ff0735221d2d4a67277cf7d
sisdream <sissipascale2@gmail.com>
2024-02-06 16:57:58 +0100

    feat: push mail

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
-// axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-// .then((response) =>{
-//     for (let i = 0; i <= 10; i++) {
-//         console.log(response.data.response); 
+// const { createApp } = Vue;
+// const app = createApp({
+//     data: {
+//       emails : [],
+//     },
+//     mounted(){
+//         for (let i = 0; i < 10; i++) {
+//             axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+//             .then((response) =>{
+//                 console.log(response.data.response);
+//                 // const email = response.data.response;
+//                 // this.emails.push(response.data);
+//             }); 
+//         };
 //     }
 // });
+// app.mount('#app');
 
 
-for (let i = 0; i <= 10; i++) {
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-    .then((response) =>{
-     console.log(response.data.response);
-    }); 
-};
+const { createApp } = Vue;
+
+const app = createApp({
+    data(){
+        return{
+            emails : [],
+        };
+    },
+    methods: {
+        
+    },
+    mounted(){
+        for (let i = 0; i < 10; i++) {
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+            .then((response) =>{
+                const email = response.data.response;
+                console.log(email);
+                this.emails.push(email);
+            });
+        }
+    },
+});
+
+app.mount('#app');
